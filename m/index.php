5a7b0cde39a68ca60851b1b53385516575ecd26c
yxDeveloper <dilutian0415@gmail.com>
2020-09-03 13:51:47 +0800

    zhilianyun

---
+++ m/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<!-- saved from url=(0065)file:///D:/workword/HTML/%E5%AE%8C%E6%95%B4%E7%89%88/m/index.html -->
+<html style="font-size: 120px;"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+<meta content="yes" name="apple-mobile-web-app-capable">
+<meta content="telephone=no" name="format-detection">
+<title>鼎卓企业咨询服务</title>
+<meta name="description" content="网站描述">
+<meta name="keywords" content="关键词">
+<link rel="stylesheet" type="text/css" href="./images/subpage.css" tppabs="http://www.dingzhuotj.com/m/css/subpage.css">
+<link rel="stylesheet" type="text/css" href="./images/child_vip.css" tppabs="http://www.dingzhuotj.com.cn/m/css/child_vip.css">
+<script type="text/javascript" src="./images/jquery.js" tppabs="http://www.dingzhuotj.com/m/js/jquery.js"></script>
+<style>
+.head {
+	background: #ba292e
+}
+.foot {
+	background: #323232
+}
+.nav {
+	background: #ba292e !important
+}
+.nav_color {
+	background: #ba292e
+}
+</style>
+<link rel="stylesheet" type="text/css" href="./images/master.css" tppabs="http://www.dingzhuotj.com/m/css/master.css">
+<link rel="stylesheet" type="text/css" href="./images/swiper.css" tppabs="http://www.dingzhuotj.com/m/css/swiper.css">
+<script type="text/javascript" src="./images/nav.js" tppabs="http://www.dingzhuotj.com/m/js/nav.js"></script>
+<script type="text/javascript" src="./images/cart_icon.js" tppabs="http://www.dingzhuotj.com/m/js/cart_icon.js"></script>
+<script type="text/javascript" src="./images/swiper.js" tppabs="http://www.dingzhuotj.com/m/js/swiper.js"></script>
+<script type="text/javascript" src="./images/lihe.js" tppabs="http://www.dingzhuotj.com/m/js/lihe.js"></script>
+</head>
+
+<body>
+<ul class="nav">
+  <li><a href="http://www.dingzhuotj.com/m/index.html" tppabs="http://www.dingzhuotj.com/m/index.php" title="首页"><span class="iconfont"></span>首页</a></li>
+  
+  <li><a href="http://www.dingzhuotj.com/m/list.php-tid=1.htm" tppabs="http://www.dingzhuotj.com/m/list.php?tid=1" title="关于我们">关于我们</a></li>
+  
+  <li><a href="http://www.dingzhuotj.com/m/list.php-tid=2.htm" tppabs="http://www.dingzhuotj.com/m/list.php?tid=2" title="新闻资讯">新闻资讯</a></li>
+  
+  <li><a href="file://http://www.dingzhuotj.com/m/list.php-tid=6.htm" tppabs="http://www.dingzhuotj.com/m/list.php?tid=6" title="项目案例">项目案例</a></li>
+  
+  <li><a href="file://http://www.dingzhuotj.com/m/list.php-tid=10.htm" tppabs="http://www.dingzhuotj.com/m/list.php?tid=10" title="常见问题">常见问题</a></li>
+  
+  <li><a href="file://http://www.dingzhuotj.com/m/list.php-tid=11.htm" tppabs="http://www.dingzhuotj.com/m/list.php?tid=11" title="联系我们">联系我们</a></li>
+  
+</ul>
+<div class="allpage">
+  <div class="black-fixed iconfont"></div>
+  <div class="header">
+    <div class="head"> <a href="http://www.dingzhuotj.com/m/index.html" tppabs="http://dingzhuotj.com/m/index.php" title="" class="logo"> <img src="./images/logo.png" tppabs="http://dingzhuotj.com/m/images/logo.png"> </a>
+      <div class="nav-btn iconfont"></div>
+    </div>
+  </div>
+  <div class="content content_new">
+    <div class="banner">
+      <div class="swiper-container swiper-container-horizontal">
+        <div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(-614px, 0px, 0px);"><div class="swiper-slide swiper-slide-duplicate swiper-slide-prev" data-swiper-slide-index="1" style="width: 614px;"> <img src="./images/1-1F31G256300-L.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G256300-L.jpg"> </div>
+          <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="0" style="width: 614px;"> <img src="./images/1-1F31G256400-L.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G256400-L.jpg"> </div>
+<div class="swiper-slide swiper-slide-next" data-swiper-slide-index="1" style="width: 614px;"> <img src="./images/1-1F31G256300-L.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G256300-L.jpg"> </div>
+
+        <div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="0" style="width: 614px;"> <img src="./images/1-1F31G256400-L.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G256400-L.jpg"> </div></div>
+        dingzhuotj.com
+        <!-- Add Pagination -->
+        
+        <div class="swiper-pagination swiper-pagination-clickable"><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span><span class="swiper-pagination-bullet"></span></div>
+      </div>
+    </div>
+    <div class="common_i_title"> <span>PRODUCTS</span>
+      <h2>产品中心</h2>
+    </div>
+    <div class="product_i common_i_css">
+      <div class="product_i_list">
+        <ul>
+          <li> <a href="http://www.dingzhuotj.com/m/view.php-aid=12.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=12"> <img src="./images/1-1F31G05F1913-lp.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G05F1913-lp.jpg" alt=""> <span>拓展训练</span></a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=11.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=11"> <img src="./images/1-1F31G05524557-lp.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G05524557-lp.jpg" alt=""> <span>企业培训</span></a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=10.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=10"> <img src="./images/1-1F31G054451A-lp.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G054451A-lp.jpg" alt=""> <span>亲子沙盘</span></a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=9.htm" tppabs="http://dingzhuotj.comn/m/view.php?aid=9"> <img src="./images/1-1F31G054123I-lp.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G054123I-lp.jpg" alt=""> <span>代理记账</span></a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=8.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=8"> <img src="./images/1-1F31G05244325-lp.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G05244325-lp.jpg" alt=""> <span>内训</span></a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=7.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=7"> <img src="./images/1-1F31G05144R2-lp.jpg" tppabs="http://dingzhuotj.com/uploads/allimg/170317/1-1F31G05144R2-lp.jpg" alt=""> <span>工商登记代理代办服务</span></a> </li>
+
+        </ul>
+        <div class="c"></div>
+      </div>
+    </div>
+    <div class="more_i"><a href="http://www.dingzhuotj.com/m/list.php-tid=6.htm" tppabs="http://dingzhuotj.com/m/list.php?tid=6"> 查看更多 </a></div>
+    <div class="common_i_title"> <span>LATEST NEWS</span>
+      <h2>新闻资讯</h2>
+    </div>
+    <div class="news_i common_i_css">
+      <div class="news_i_list">
+        <ul>
+          <li> <a href="http://www.dingzhuotj.com/m/view.php-aid=15.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=15" title="企业培训"> <span>2017-03-17</span>企业培训</a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=13.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=13" title="拓展训练"> <span>2017-03-17</span>拓展训练</a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=14.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=14" title="亲子沙盘"> <span>2017-03-17</span>亲子沙盘</a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=2.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=2" title="内训"> <span>2017-03-16</span>内训</a> </li>
+<li> <a href="http://www.dingzhuotj.com/m/view.php-aid=1.htm" tppabs="http://dingzhuotj.com/m/view.php?aid=1" title="代理记账"> <span>2017-03-16</span>代理记账</a> </li>
+
+        </ul>
+      </div>
+    </div>
+    <div class="more_i"><a href="http://www.dingzhuotj.com/m/list.php-tid=2.htm" tppabs="http://dingzhuotj.com/m/list.php?tid=2"> 查看更多 </a></div>
+    
+    <div class="baoming">
+      <div class="baoming_title">
+        <h2>联系我们</h2>
+        <span>地址：天津市和平区南京路305经济联合中心大厦705</span> </div>
+      <div class="baoming_c"> <span class="iconfont2">tel:</span> 15802288940 <a href="tel:15802288940">立即预约</a> </div>
+    </div>
+    <div class="beian"> 技术支持：<a title="智链云(天津)信息科技发展有限公司" target="_blank" href="http://www.dingzhuotj.com/m/index.html#">智链云(天津)信息科技发展有限公司</a>　 </div>
+  </div>
+  <div class="footer">
+    <div class="foot foot-relative" id="foot">
+      <div class="foot-relative"> <a href="tel:15802288940" title="电话"> <span class="commonfont"></span>
+        <h3>电话</h3>
+        </a><a href="sms:15302076529" title="短信"> <span class="commonfont"></span>
+        <h3>短信</h3>
+        </a><a href="http://www.dingzhuotj.com" title="分享"> <span class="commonfont"></span>
+        <h3>分享</h3>
+        </a> </div>
+    </div>
+  </div>
+</div>
+
+</body></html>
